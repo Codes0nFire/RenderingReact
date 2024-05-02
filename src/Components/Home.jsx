791dd0f5c28d3b7cd516453b589f2b589ad3cebd
NoN Responsive WebDeveloper <gg.georgemail50a@gmail.com>
2024-05-02 07:25:28 +0530

    newApicall with pagination using useEffect hook

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import axios from "../helpers/axios"
 import { useState } from 'react'
 
+
 const Home = () => {
 
-  const [users,setusers]=useState([])
+  let[images,setimages]= useState([]);
+  let [page,setpage]=useState(1);
 
   
-const findUsers= async ()=>{
+const getimages= async ()=>{
 
 
  try {
-  const {data}=await axios.get("/users");
-   setusers(data);
+  const {data}=await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=12`);
+   
+  setimages(data);
+  console.log(images);
+  
+  
    
    
    
@@ -31,37 +37,46 @@ const findUsers= async ()=>{
 }
 
 
-var filluserdata= <h1>Loading users .......</h1>
+var filluserdata= <h1 className='text-white' >Loading images .......</h1>
 
-if(users.length>0){
-  filluserdata= users.map(user=> {
-    return <div key={user.id} className='w-full  border  flex flex-col gap-2 px-5  ' >
+if(images.length>0){
+  filluserdata= images.map(image=> {
+    return <div key={image.id}   >
       
-      <div>
-        <h1 className='text-xl' >Name={user.name}</h1>
-        <h3 className='text-sm' >@{user.username}</h3>
-      <h3> Email={user.email}</h3>
-      </div>
+      
+        <img className='w-72 h-72 rounded-md' src={image.download_url} alt="" />
+        <h1 className='text-sm opacity-70 text-white' >Author:@{image.author}</h1>
+        
+     
+      
       
     </div>
   } );
 }
 
 
+useEffect(()=>{
+  getimages();
 
+},[page])
 
 
 
 
 
   return (
-    <div className='flex items-center justify-center flex-col'>
-      <h1>Home Page</h1>
+    <div className='flex flex-wrap pb-5'>
+      <h1 className='text-white' >Home Page</h1>
       <br />
-      <button className=' btn px-4 py-2 border border-red-500 rounded-lg' onClick={findUsers}>Get Users</button>
+      {/* <button className=' btn px-4 py-2 border border-red-500 rounded-lg' onClick={findUsers}>Get Users</button> */}
 
-      <div className='pt-5 w-screen'>
+      <div className='pl-10 pt-10 flex flex-wrap gap-5'>
       {filluserdata}
+      </div>
+
+      <div className='flex gap-5 mt-10 pl-10' >
+          <button onClick={()=> {if(page>1){setpage(page-1)}}} className=' text-white btn px-4 py-2 border border-white rounded-lg' >Prev</button>
+          <button onClick={()=> setpage(page+1)} className=' text-white btn px-4 py-2 border border-white rounded-lg'>Next</button>
       </div>
 
        
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -9,8 +9,8 @@ const submitHandler=()=>{
 }
 
   return (
-    <div className='flex items-center justify-center'>
-      <h1>Profile Page</h1>
+    <div className='flex text-white items-center justify-center'>
+      <h1 >Profile Page</h1>
 
       {/* <form action="">
       <button onClick={submitHandler} className='p-1 bg-black text-white text-sm rounded-md'>Home</button>
